5a091007772ebe2954e3ddfe985454b486f0dbe0
Dmitriy <dimaliahov@gmail.com>
2019-09-12 12:13:19 +0300

    Initial commit

---
+++ src/com/gmail/dimaliahov/TaskTwo.java
@@ -0,0 +1,19 @@
+package com.gmail.dimaliahov;
+public class TaskTwo {
+    public static void main(String[] args) {
+        /*
+         * Написать програму которая вычислит и выведет на екран
+         * площадь треугольника если извесны его стороны (Если известны его стороны, их не нужно вводить с клавиатуры)
+         */
+        int katetOne = 2;
+        int katetTwo = 4;
+        int katetThree = 5;
+        double polPerimetr;
+        double squareTriangle;
+
+        polPerimetr = (katetOne + katetTwo + katetThree) / 2.0;
+
+        squareTriangle = Math.sqrt(polPerimetr * (polPerimetr - katetOne) * (polPerimetr - katetTwo) * (polPerimetr - katetThree));
+        System.out.println("The area of the triangle is " + squareTriangle);
+    }
+}
